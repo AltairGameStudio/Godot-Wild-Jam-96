38abf789fcba1f4774996da415593495ee0397cd
JDomingueti <jean.domingueti@gmail.com>
2026-08-18 18:54:58 -0300

    Adicionando protótipo de inventário

---
+++ scripts/ui/slot.gd
@@ -0,0 +1,37 @@
+class_name slot
+extends Control
+
+func _ready() -> void:
+	pass
+
+func set_empty_slot() -> void:
+	$sprite.texture = null
+	$amount.text = ""
+
+func _get_drag_data(_at_position: Vector2) -> Variant:
+	var data = {
+		"sprite": $sprite.texture,
+		"amount": $amount.text,
+		"backup": self
+	}
+	set_empty_slot()
+	set_drag_preview(data.sprite)
+	return data
+
+func _can_drop_data(_at_position: Vector2, data: Variant) -> bool:
+	if data is slot:
+		return true
+	return false
+
+func _drop_data(_at_position: Vector2, data: Variant) -> void:
+	if $sprite.texture == null:
+		return
+	if $sprite.texture == data.sprite: # Se o sprite for o mesmo soma as quantidades
+		var quantity = int($amount.text)
+		quantity += int(data.amount)
+		$amount.text = str(quantity)
+	else: # Se o sprite for diferente troca os dados de lugar
+		data.backup.get_node("sprite").texture = $sprite.texture
+		data.backup.get_node("amount").text = $amount.text
+		$sprite.texture = data.sprite
+		$amount.text = data.amount
---
+++ scripts/ui/ui_canvas.gd
@@ -0,0 +1,23 @@
+extends CanvasLayer
+
+func _ready() -> void:
+	pass
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("ui_cancel"):
+		$inventario.visible = not $inventario.visible
+	pass
+
+func add_item_inventory(sprite: Texture) -> bool:
+	var empty = null
+	for inv_slot in $inventario/Container.get_children():
+		if inv_slot.get_node("sprite").texture == sprite:
+			var new_amount = int(inv_slot.get_node("amount").text)
+			new_amount += 1
+			inv_slot.get_node("amount").text = str(new_amount)
+		elif inv_slot.get_node("sprite").texture == null and empty == null:
+			empty = inv_slot
+	if not(empty == null):
+		empty.get_node("sprite").texture = sprite
+		empty.get_node("amount").text = "1"
+	return false
