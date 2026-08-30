f0a09c7f21bc9361b83d3ad3793782c904a70e4e
Pedro Henrique Coterli <pedrocoterli@gmail.com>
2026-08-30 17:15:52 -0300

    Corrigindo knockback dos inimigos

--- scripts/entities/enemy_bullet.gd
+++ scripts/entities/enemy_bullet.gd
@@ -17,7 +17,7 @@ func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		if body.has_method("take_damage"):
 			# Passa o dano e a direção para empurrar o player
-			body.take_damage(damage, direction * 150.0, true)
+			body.take_damage(damage, Vector2.ZERO, true)
 		queue_free()
 		
 	# Se bateu em paredes ou barreiras, a bala some
